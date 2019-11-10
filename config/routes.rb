cb4591e806f5c80b403c92544e2211581c10adec
Maikel Chandika <mkdika@gmail.com>
2019-11-10 21:48:13 +0700

    Generate devise views

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   
-  devise_for :users
+  devise_for :user, :path => '', :path_names => { :sign_in => "login", :sign_out => "logout" }
+
+  get "/login", to: "home#login"
+  
   scope :admin do
     get "/", to: "admin#index"
     resources :categories
